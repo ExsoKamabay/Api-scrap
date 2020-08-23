996696d606d32082b0c8c968faf7a0b39aa544ba
Exso Kamabay <45049739+ExsoKamabay@users.noreply.github.com>
2020-08-23 17:55:26 +0800

    Update __init__.py

--- scrap/__init__.py
+++ scrap/__init__.py
@@ -1,4 +1,12 @@
-
-from .enggine_search_apk import *
-from .enggine_search_exe import *
-from .github_search_file import *
+try:
+    from .enggine_search_apk import *
+    from .enggine_search_exe import *
+    from .github_search_file import *
+except:
+    from os import name,system
+    if name == "nt":
+        system("pip install bs4");
+        system("pip install requests");
+    else:
+        system("pip3 install bs4");
+        system("pip3 install requests");
