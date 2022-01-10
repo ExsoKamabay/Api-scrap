b52c2a9d44bd06eb18252facf5220b91fa59b95f
Exso Kamabay <45049739+ExsoKamabay@users.noreply.github.com>
2022-01-10 19:32:49 +0800

    Update kmy_scrap.py

--- kmy_scrap/kmy_scrap.py
+++ kmy_scrap/kmy_scrap.py
@@ -1,5 +1,7 @@
 import json,requests,random
-from urllib.parse import quote_plus;from bs4 import BeautifulSoup;
+from time import sleep as timeout
+from youtube_search import YoutubeSearch
+from urllib.parse import quote_plus;from bs4 import BeautifulSoup
 
 def random_user_agent():
     return {'User-Agent':random.choice((
@@ -13,47 +15,59 @@ def random_user_agent():
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
     ))}
-
+    
 
 class Github:
-    def __init__(self,query='exso kamabay',search_page=None):
+    def __init__(self,query='exso kamabay'):
         self.query = quote_plus(query)
-        self.search_page   =    search_page;
-
-    def result(self,url):
-        self.lst = [];
-        self.get = requests.get(url,headers=random_user_agent()).content
-        self.soup = BeautifulSoup(self.get,'html.parser')
-        for url,dec,star,lang,up in zip(
-            self.soup.find_all('a',{'class':'v-align-middle'}),
-            self.soup.find_all('p',{'class':'mb-1'}),
-            self.soup.find_all('a',{'class':'Link--muted'}),
-            self.soup.find_all('span',{'itemprop':'programmingLanguage'}),
-            self.soup.find_all('relative-time',{'class':'no-wrap'}),
-            ):
-            self.lst.append({
-                'url':f"https://github.com{url['href']}",
-                'star':star.text.replace(' ','').strip('\n').replace('\n',' '),
-                'upload':up.text,
-                'language':lang.text,
-                'decription':dec.text.replace('    ','').strip('\n').replace('\n',''),
-                })
-        return self.lst;
+        self.url = f"https://github.com/search?p=%s&q={self.query}&type=Repositories"
 
     @property
     def total_results(self):
-        self.check = BeautifulSoup(requests.get(f'https://github.com/search?q={self.query}',headers=random_user_agent()).content,'html.parser')
-        self.find_ = self.check.find('div',{'class','d-flex flex-column flex-md-row flex-justify-between border-bottom pb-3 position-relative'})
-        return (self.find_.h3.text.replace('   ','').replace('\n',''))
+        try:
+            self.get = requests.get(self.url%(1),headers=random_user_agent())
+            if self.get.status_code != 200:
+                return 'Invalid response! %s'%(self.get.status_code)
+            else:
+                self.soup = BeautifulSoup(self.get.content,'html.parser')
+                self.find_ = self.soup.find('div',{'class','d-flex flex-column flex-md-row flex-justify-between border-bottom pb-3 position-relative'})
+                return (self.find_.h3.text.replace('   ','').replace('\n',''))
+        except:return 'Check your internet connection!'
 
-    @property
-    def results(self):
+    def search(self,page:int):
+        try:
+            self.lst = [];
+            self.get = requests.get(self.url%(page),headers=random_user_agent())
+            if self.get.status_code == 200:
+                self.soup = BeautifulSoup(self.get.content,'html.parser')
+                for url,dec,star,lang,up in zip(
+                    self.soup.find_all('a',{'class':'v-align-middle'}),
+                    self.soup.find_all('p',{'class':'mb-1'}),
+                    self.soup.find_all('a',{'class':'Link--muted'}),
+                    self.soup.find_all('span',{'itemprop':'programmingLanguage'}),
+                    self.soup.find_all('relative-time',{'class':'no-wrap'}),
+                    ):
+                    self.lst.append({
+                        'url':f"https://github.com{url['href']}",
+                        'star':star.text.replace(' ','').strip('\n').replace('\n',' '),
+                        'upload':up.text,
+                        'language':lang.text,
+                        'decription':dec.text.replace('    ','').strip('\n').replace('\n',''),
+                        })
+                return json.dumps({"search":self.lst},indent=2,sort_keys=True)
+            else:return "Invalid response %s"%(self.get.status_code)
+        except:pass
+
+    def searches(self,page:int):
+        self.lst = []
         self.list = []
-        for i in range(self.search_page):
-            self.value = self.result(f'https://github.com/search?p={i+1}&q={self.query}&type=Repositories')
+        for i in range(page):
+            self.value = self.search(self.url%(i+1))
             self.list.append(self.value)
-        return json.dumps({"Results":self.list},indent=2)
-
+        for y in self.list:
+            for x in json.loads(y)['search']:
+                self.lst.append(x)
+        return json.dumps({"searches":self.lst},indent=2,sort_keys=True)
 
 
 class Google:
@@ -66,14 +80,13 @@ class Google:
         self.urli = f"https://www.google.com/search?q={self.query}&tbm=isch"
 
 
-    def get_content(self,url):
+    def get_content(self,url) -> str:
         """Mengambil konten alamat url"""
         try:
             self.url = requests.get(url,headers=random_user_agent())
             if self.url.status_code == 200:return self.url.content
             else:return f"Response status code {self.url.status_code}"
         except:return "Check koneksi internet!"
-
 
     def search_image(self,**kwargs):
         """
@@ -95,7 +108,8 @@ class Google:
         """
         def related_results():
             self.lst_rst = []
-            if not kwargs:pass
+            if not kwargs:
+                return "no keywords!";
             else:
                 for k,v in kwargs.items():
                     if k in ('hl','cr'):
@@ -135,15 +149,57 @@ class Google:
                     },indent=1)
         else:return json.dumps({'images':search_results()},indent=1)
 
+    def search_query(self,**kwargs):
+        """
+        start : int -> start search
+        stop : int -> stop search
+        lang : str -> language query
+        country : str -> country code
+        """
+        if not kwargs:
+            return "no keywords!";
+        else:
+            self.results = []
+            def filter_(url):
+                remove = url.strip('/url?q=')
+                find = remove.find('&sa=U&ved=')
+                return remove[:find]
+            self.dict = dict({'lang':'hl','country':'cr','start':'start','stop':'num'})
+            for k,v in kwargs.items():
+                if k in self.dict.keys():
+                    self.urlq += "&%s=%s"%(self.dict[k],v)
+                else:pass
+            self.soup = BeautifulSoup(self.get_content(self.urlq),'html.parser')
+            for u,d,t in zip(
+                self.soup.find_all('div',{'class':'kCrYT'}),
+                self.soup.find_all('div',{'class':'BNeawe s3v9rd AP7Wnd'}), 
+                self.soup.find_all('div',{'class':'BNeawe vvjwJb AP7Wnd'})
+                ):
+                if not 'pause' in kwargs.keys():pass
+                else:timeout(kwargs['pause'])
+                try:
+                    # result
+                    self.results.append({'url':filter_(u.a['href']),'title':t.text,'decription':d.text})
+                except:pass
+            if not self.results:
+                return "sorry an error occurred please try again!"
+            else:return json.dumps(self.results,indent=1,sort_keys=True)
+
+    def search_video(self,max_search=5):
+        "maximal search"
+        self.query_ = self.query.replace('+',' ')
+        self.results = YoutubeSearch(self.query_,max_search)
+        return json.dumps(self.results.to_dict(),indent=2)
+
 
 class Search_App_Mod:
+    'query string'
     def __init__(self,query:str) -> str:
-        self.query = quote_plus(query)
-    
-    @property
-    def happymod(self):
+        self.query = quote_plus(query);
+
+    def happymod(self) -> None:
         self.results = []
-        try:self.gets = requests.get(f'https://happymod.com/search.html?q={self.query}',headers=random_user_agent())
+        try:self.gets = requests.get(f'https://happymod.com/search.html?q={self.query}',params=random_user_agent())
         except:return "Check Your Internet Connection!"
         self.urls = BeautifulSoup(self.gets.content,'html.parser').find_all('a',{'class':'pdt-app-img'})
         for i in self.urls:
@@ -152,11 +208,12 @@ class Search_App_Mod:
             self.url = BeautifulSoup(
                 requests.get(f"https://happymod.com{i['href']}",
                 headers=random_user_agent()).content,'html.parser');
-            for name,info,ftur,img in zip(
+            for name,info,ftur,img,link in zip(
                 self.url.find_all('div',{'class':'new-div-box new-pdt-bg-box'}),
                 self.url.find_all('ul',{'class':'new-pdt-ul clearfix'}),
                 self.url.find_all('div',{'class':'new-pdt-msg-box'}),
-                self.url.find_all('img',{'class':'lazy'})
+                self.url.find_all('img',{'class':'lazy'}),
+                self.url.find_all('a',{'class':'download-btn'})
                 ):
                 self.results.append([{
                     "App":i['title'],
@@ -165,27 +222,28 @@ class Search_App_Mod:
                         'icon':img['data-original'],
                         'package':self.ipk[self.rpk:].strip('/'),
                         'detail':info.text.replace('-','').strip('\n').replace('\n',','),
+                        'download':'https://happymod.com'+link['href'],
                         'decription':ftur.p.text.replace('\t','').replace('\n',' ').replace('\u221a','').replace('\r','')
                     }]
                     }])
         return json.dumps({"Results":self.results},indent=1)
     
-    def rexdl(self,search_page:int):
+    def rexdl(self,page:int) -> int:
         self.ls_results = []
         def search_url():
             results = [];
-            for i in range(search_page):
+            for i in range(page):
                 for u in BeautifulSoup(requests.get(f"https://rexdl.com/page/{i+1}/?s={self.query}",
-                    headers=random_user_agent()).content,'html.parser').find_all('h2',{'class':'post-title'}):
+                    params=random_user_agent()).content,'html.parser').find_all('h2',{'class':'post-title'}):
                     results.append({'url':u.a['href'],'title':u.a['title']})
             return results;
         for i in search_url():
-            self.soup = BeautifulSoup(requests.get(i['url'],headers=random_user_agent()).content,'html.parser')
+            self.soup = BeautifulSoup(requests.get(i['url'],params=random_user_agent()).content,'html.parser')
             for dec,nw,ld in zip(
                 self.soup.find_all('div',{'class':'entry-inner'}),
                 self.soup.find_all('div',{'class':'DWPxHb'}),
                 self.soup.find_all('span',{'class':'readdownload a'})):
-                self.link_i = BeautifulSoup(requests.get(ld.a['href'],headers=random_user_agent()).content,'html.parser')
+                self.link_i = BeautifulSoup(requests.get(ld.a['href'],params=random_user_agent()).content,'html.parser')
                 for up,vr,fs,pw in zip(
                     self.link_i.find_all('li',{'class':'dl-update'}),
                     self.link_i.find_all('li',{'class':'dl-version'}),
@@ -204,3 +262,4 @@ class Search_App_Mod:
                             'size':fs.text.strip('File Size:'),
                             'password':pw['title']}]})
         return json.dumps(self.ls_results,indent=1)
+
